9fd65a0706f743fa036cf441f236faa840a15213
vcatafesta <vcatafesta@gmail.com>
2024-09-27 13:42:30 -0400

    sex 27 set 2024 13:42:30 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- pkgbuild/pkg/chili-livecd-key/usr/bin/chili-livecd-key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-test ! -d /etc/pacman.d/gnupg && mkdir -p /etc/pacman.d/gnupg
-test ! -e /etc/pacman.d/gnupg/pubring.gpg && cp -a /usr/share/pacman/keyrings/pubring.gpg /etc/pacman.d/gnupg/pubring.gpg
-test ! -e /etc/pacman.d/gnupg/pubring.kbx && cp -a /usr/share/pacman/keyrings/pubring.kbx /etc/pacman.d/gnupg/pubring.kbx
-test ! -e /etc/pacman.d/gnupg/trustdb.gpg && cp -a /usr/share/pacman/keyrings/trustdb.gpg /etc/pacman.d/gnupg/trustdb.gpg
