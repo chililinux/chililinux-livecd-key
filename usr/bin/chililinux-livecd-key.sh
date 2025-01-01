65bdd7269338e3de8c71d0ff018e049fed8eec6c
vcatafesta <vcatafesta@gmail.com>
2025-01-01 17:21:24 -0400

    qua 01 jan 2025 17:21:24 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- usr/bin/chililinux-livecd-key.sh
+++ usr/bin/chililinux-livecd-key.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-# shellcheck shell=bash disable=SC1091,SC2039,SC2166
 
-if test ! -e /etc/pacman.d/gnupg/pubring.gpg; then
-  mkdir -p /etc/pacman.d/gnupg
-  cp -a /usr/share/pacman/keyrings/pubring.gpg /etc/pacman.d/gnupg/pubring.gpg
-  cp -a /usr/share/pacman/keyrings/trustdb.gpg /etc/pacman.d/gnupg/trustdb.gpg
-fi
+msg() {
+  local ALL_OFF="\e[1;0m"
+  local BOLD="\e[1;1m"
+  local GREEN="${BOLD}\e[1;32m"
+
+  local MESSAGE=$1
+  shift 1
+  printf "${GREEN}==>[chililinux-livecd-key]${ALL_OFF}${BOLD} ${MESSAGE}${ALL_OFF}\n" "$@" >&2
+}
+
+# Lista dos arquivos a serem copiados
+files=('pubring.gpg' 'trustdb.gpg')
+
+# Caminhos de origem e destino
+srcpath='/usr/share/pacman/keyrings'
+destpath='/etc/pacman.d/gnupg'
+
+# Verifica se o diretório de destino existe, caso contrário, cria-o
+[[ ! -d "$destpath" ]] && mkdir -p "$destpath"
+
+# Itera pelos arquivos da lista
+for file in "${files[@]}"; do
+	srcfile="$srcpath/$file"
+	destfile="$destpath/$file"
+
+	# Verifica se o arquivo existe na origem
+	if [[ -e "$srcfile" ]]; then
+    msg "Copying 'Public Keyring' and 'Trust Database' ${srcfile} into ${destfile}..."
+    cp -af "$srcfile" "$destfile" >/dev/null 2>&1
+  fi
+done
+
+# Garante que o chaveiro está inicializado corretamente
+msg "Initializing pacman keyring..."
+pacman-key --init >/dev/null 2>&1
+msg "Done!"
